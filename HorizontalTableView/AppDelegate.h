e6b468150c12eed29446fa0c3062f8ea97c34ae3
SupermanGC <517255198@qq.com>
2015-08-14 10:59:20 +0800

    HorizontalTableView
    一个简单的横向TableView，可做简单参考

---
+++ HorizontalTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HorizontalTableView
+//
+//  Created by qianfeng007 on 15/8/14.
+//  Copyright (c) 2015年 高翀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HorizontalTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HorizontalTableView
+//
+//  Created by qianfeng007 on 15/8/14.
+//  Copyright (c) 2015年 高翀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
